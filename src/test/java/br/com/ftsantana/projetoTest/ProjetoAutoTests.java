da28235e09038839c8b22406e8cf4f494227286a
Filipe Santana <filipe.t.santana@gmail.com>
2023-09-23 21:50:18 -0300

    configurando testes automatizados para a tela de login, teste para colocar usuario invalido e valido

--- src/test/java/br/com/ftsantana/projetoTest/ProjetoAutoTests.java
+++ src/test/java/br/com/ftsantana/projetoTest/ProjetoAutoTests.java
@@ -3,17 +3,22 @@ package br.com.ftsantana.projetoTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 @DisplayName("Projeto para Testes Automatizados")
 public class ProjetoAutoTests {
     @Test
     @DisplayName("Fazendo Login na Tela")
-    public void testFazendoLoginNaTela(){
+    public void testLoginNaTelaUsuarioValido(){
         //Abrir Chrome
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -22,9 +27,9 @@ public class ProjetoAutoTests {
 
         //Abrir o site
         navegador.get("http://localhost:4200/");
-        //Vou fazer o preenchimento da variavel login
+        //Vou fazer o preenchimento da variavel login com usuario valido
         navegador.findElement(By.id("username")).sendKeys("usuario1");
-        //Vou fazer o preenchimento da variavel senha
+        //Vou fazer o preenchimento da variavel senha com senha valida
         navegador.findElement(By.id("password")).sendKeys("1234");
         //Vou clicar em confirmar
         navegador.findElement(By.id("botaoDeLogin")).click();
@@ -32,7 +37,7 @@ public class ProjetoAutoTests {
 
     @Test
     @DisplayName("Fazendo Login na Tela")
-    public void testLoginNaTelaComErro(){
+    public void testLoginNaTelaUsuarioInvalido(){
         //Abrir Chrome
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -41,12 +46,45 @@ public class ProjetoAutoTests {
 
         //Abrir o site
         navegador.get("http://localhost:4200/");
-        //Vou fazer o preenchimento da variavel login
+        //Vou fazer o preenchimento da variavel login com usuario invalido
         navegador.findElement(By.id("username")).sendKeys("usuario4");
-        //Vou fazer o preenchimento da variavel senha
+        //Vou fazer o preenchimento da variavel senha com senha invalida
         navegador.findElement(By.id("password")).sendKeys("12345");
         //Vou clicar em confirmar
         navegador.findElement(By.id("botaoDeLogin")).click();
+    }
+
+    @Test
+    @DisplayName("Fazendo Login na Tela")
+    public void testLoginNaTelaUsuarioInvalidoDepoisValido(){
+        //Abrir Chrome
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver navegador = new ChromeDriver(options);
+
+        //Abrir o site
+        navegador.get("http://localhost:4200/");
+        //Vou fazer o preenchimento da variavel login com usuario invalido
+        navegador.findElement(By.id("username")).sendKeys("usuario4");
+        //Vou fazer o preenchimento da variavel senha com senha invalida
+        navegador.findElement(By.id("password")).sendKeys("12345");
+        //Vou clicar em confirmar
+        navegador.findElement(By.id("botaoDeLogin")).click();
+
+        WebDriverWait wait = new WebDriverWait(navegador, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        Alert alert = navegador.switchTo().alert();
+        alert.accept();
+
+        //Vou fazer o preenchimento da variavel login com usuario valido
+        navegador.findElement(By.id("username")).sendKeys("usuario1");
+        //Vou fazer o preenchimento da variavel senha com senha valida
+        navegador.findElement(By.id("password")).sendKeys("1234");
+        //Vou clicar em confirmar
+        navegador.findElement(By.id("botaoDeLogin")).click();
+
     }
 
 }
